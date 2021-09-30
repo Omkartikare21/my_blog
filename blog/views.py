c7e2578272ed51efc7f76a34701296ac6ca155e3
Omkar <omkartikare55@gmail.com>
2021-09-30 03:33:57 -0700

    Finishing touches and data

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse
 class StartingPageView(ListView):
     template_name = "blog/index.html"
     model = Posts
-    ordering = ['-date']
+    ordering = ['-date', "-id"]
     context_object_name = "posts"
 
     def get_queryset(self):
@@ -19,25 +19,11 @@ class StartingPageView(ListView):
         return data
 
 
-# def starting_page(request):
-#     latest_posts = Posts.objects.all().order_by("-date")[:3]
-#     return render(request, 'blog/index.html', {
-#         "posts": latest_posts
-#     })
-
-
 class AllPostsView(ListView):
     model = Posts
     template_name = "blog/all-posts.html"
-    ordering = ['-date']
+    ordering = ['-date', '-id']
     context_object_name = "all_posts"
-
-
-# def posts(request):
-#     all_posts = Posts.objects.all().order_by("-date")
-#     return render(request, "blog/all-posts.html", {
-#         "all_posts": all_posts
-#     })
 
 
 class PostDetailView(View):
@@ -79,24 +65,6 @@ class PostDetailView(View):
                 "saved_for_later": self.is_stored_post(request, post.id)
             }
             return render(request, "blog/post-detail.html", context)
- # thebelow code works for only GET method and not POST method, so we have to do it manually , see the above code
-        # template_name = "blog/post-detail.html"
-        # model = Posts
-        # context_object_name = "post"
-
-        # def get_context_data(self, **kwargs):
-        #     context = super().get_context_data(**kwargs)
-        #     context['post_tags'] = self.object.tags.all()
-        #     context['comment_form'] = CommentForm() # creating comment section on the existing template by importing and instantiating
-        #                                             # add 'comment_form' in post-detail.html and add a button to save it
-        #     return context
-
-        # def post_details(request, slug):
-        #     identified_post = get_object_or_404(Posts, slug=slug)
-        #     return render(request, "blog/post-detail.html", {
-        #         "post": identified_post,
-        #         "post_tags": identified_post.tags.all()
-        #       })
 
 
 class ReadLaterView(View):
